72790ef70a72d3de1b1807883b3d27e89c03b0bf
zimogg <18611696761@163.com>
2018-12-30 19:23:51 +0800

    submit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
